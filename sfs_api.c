8e0ea996621288bf378fc141625ba9b9379d7a76
Braedon McDonald <braedonmcdonald1@gmail.com>
2019-11-29 12:08:05 -0500

    move variables from sfs_mksfs to global defines add utility functions for interacting with the freemap and open file descriptor table start implementing sfs_fopen

--- sfs_api.c
+++ sfs_api.c
@@ -1,7 +1,19 @@
 #include "sfs_api.h"
 #include "disk_emu.h"
+#include "root_dir_cache.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+#define BLOCK_SIZE 1024
+#define NUM_BLOCKS 8258 // 1 super block
+                        // 64 blocks for inode table
+                        // 8192 data blocks
+                        // 1 free bitmap block
+#define INODE_TABLE_LENGTH 64
+#define ROOT_DIR_INODE_NUM 0
+#define MAX_OPEN_FILES 100
+
+void init_open_file_descriptor_table();
 
 typedef struct SUPER_BLOCK {
     int magic_number;
@@ -31,27 +43,30 @@ typedef struct INODE {
 
 INODE inode_table_cache[512];
 
+typedef struct OPEN_FILE_DESCRIPTOR_TABLE_ENTRY {
+    int valid;
+    int inode_num;
+    int rptr;
+    int wptr;
+} OPEN_FILE_DESCRIPTOR_TABLE_ENTRY;
+
+OPEN_FILE_DESCRIPTOR_TABLE_ENTRY open_file_descriptor_table[MAX_OPEN_FILES];
+
 void mksfs(int fresh) 
 {
-    int block_size = 1024;
-    int num_blocks = 8258; // 1 super block
-                           // 64 blocks for inode table
-                           // 8192 data blocks
-                           // 1 free bitmap block
-    int inode_table_length = 64;
-    SUPER_BLOCK super_block;
     INODE root_dir_inode;
+    SUPER_BLOCK super_block;
 
     if (fresh)
     {
         // init with 8 megabytes of free space
-        init_fresh_disk("emulated_disk", block_size, num_blocks);
+        init_fresh_disk("emulated_disk", BLOCK_SIZE, NUM_BLOCKS);
 
         // write super block to first block of disk
         super_block.magic_number = 0xABCD0005;
-        super_block.block_size = block_size;
-        super_block.fs_size = num_blocks;
-        super_block.inode_table_length = inode_table_length;
+        super_block.block_size = BLOCK_SIZE;
+        super_block.fs_size = NUM_BLOCKS;
+        super_block.inode_table_length = INODE_TABLE_LENGTH;
         super_block.root_dir_inode_num = 0;
         write_blocks(0, 1, &super_block);
 
@@ -66,10 +81,10 @@ void mksfs(int fresh)
     }
     else
     {
-        init_disk("emulated_disk", block_size, num_blocks);
+        init_disk("emulated_disk", BLOCK_SIZE, NUM_BLOCKS);
 
         // read super block
-        void *super_block_buff = (void*) malloc(block_size); // 
+        void *super_block_buff = (void*) malloc(BLOCK_SIZE); // 
         read_blocks(0, 1, super_block_buff);
         super_block = *((SUPER_BLOCK*) super_block_buff);
         free(super_block_buff);
@@ -78,6 +93,26 @@ void mksfs(int fresh)
         if (super_block.magic_number != 0xABCD0005)
         {
             printf("error reading magic number in super block\n");
+            exit(1);
+        }
+        if (super_block.block_size != BLOCK_SIZE)
+        {
+            printf("super block has wrong block size\n");
+            exit(1);
+        }
+        if (super_block.fs_size != NUM_BLOCKS)
+        {
+            printf("super block has wrong file system size\n");
+            exit(1);
+        }
+        if (super_block.inode_table_length != INODE_TABLE_LENGTH)
+        {
+            printf("super block has wrong inode table length\n");
+            exit(1);
+        }
+        if (super_block.root_dir_inode_num != ROOT_DIR_INODE_NUM)
+        {
+            printf("super block has wrong root directory inode number\n");
             exit(1);
         }
     }
@@ -89,6 +124,9 @@ void mksfs(int fresh)
     root_dir_inode = inode_table_cache[super_block.root_dir_inode_num];
 
     // cache root directory
+
+    // init open file descriptor table
+    init_open_file_descriptor_table();
 }
 
 int sfs_getnextfilename(char *fname)
@@ -103,6 +141,18 @@ int sfs_getfilesize(const char* path)
 
 int sfs_fopen(char *name)
 {
+    // create new file if it doesn't exist
+    if (rdc_get_inode_num(name) < 0)
+    {
+        // check if need to allocate new block in dir table
+        INODE root_inode = inode_table_cache[ROOT_DIR_INODE_NUM];
+        int bytes_left_in_block = BLOCK_SIZE - (root_inode.size % BLOCK_SIZE);
+        if (bytes_left_in_block < sizeof(DIR_ENTRY))
+        {
+            // check if there's enough disk space to allocate a new block
+        }
+    }
+
     return 1;
 }
 
@@ -134,4 +184,82 @@ int sfs_fread(int fileID, char *buf, int length)
 int sfs_remove(char *file)
 {
     return 1; 
+}
+
+
+/***************** utility functions *****************/
+
+// returns 1 on success
+int fm_is_available(int blocks_requested)
+{
+    int freemap[BLOCK_SIZE / 4];
+    int free_blocks_found = 0;
+
+    // read free map
+    read_blocks(1 + INODE_TABLE_LENGTH, 1, freemap);
+
+    // query each index of the freemap until enough free blocks are found
+    for (int i = 0; i < BLOCK_SIZE; i++)
+    {
+        // test the appropriate bit in the freemap
+        if (freemap[i/32] & (1 << (i%32)))
+            free_blocks_found++;
+        // stop looking for free blocks when enough have been found
+        if (free_blocks_found == blocks_requested)
+            break;
+    }
+
+    return free_blocks_found == blocks_requested;
+}
+
+// return -1 if no space
+int fm_get_next_address_and_allocate()
+{
+    int next_address = -1;
+    int freemap[BLOCK_SIZE / 4];
+
+    // read free map
+    read_blocks(1 + INODE_TABLE_LENGTH, 1, freemap);
+
+    // query each index of the freemap until a free block is found
+    int i;
+    for (i = 0; i < BLOCK_SIZE; i++)
+    {
+        // test the appropriate bit in the freemap
+        if (freemap[i/32] & (1 << (i%32)))
+        {
+            // set the ith bit in the freemap and write it to the disk
+            freemap[i/32] = freemap[i/32] | (1 << i%32);
+            write_blocks(1 + INODE_TABLE_LENGTH, 1, freemap);
+
+            // need to add the offset to the address of the first data block
+            next_address = 1 + INODE_TABLE_LENGTH + 1 + i;
+            break; // stop searching when a free block is found
+        }
+    }
+
+    return next_address;
+}
+
+void init_open_file_descriptor_table()
+{
+    // make sure every entry is set to invalid
+    for (int i = 0; i < MAX_OPEN_FILES; i++)
+        open_file_descriptor_table[i].valid = 0;
+}
+
+int get_next_fd()
+{
+    int next_fd = -1;
+
+    for (int i = 0; i < MAX_OPEN_FILES; i++)
+    {
+        if (!open_file_descriptor_table[i].valid)
+        {
+            next_fd = i;
+            break; // stop searching when the first available entry is found
+        }
+    }
+
+    return next_fd;
 }
